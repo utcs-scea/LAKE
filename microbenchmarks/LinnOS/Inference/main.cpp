8790a8d94fedda8e61b4e32eef23b75e16cab553
Isha Tarte <itarte@zemaitis.csres.utexas.edu>
2022-04-12 20:17:51 +0000

    doing for one batch

--- microbenchmarks/LinnOS/Inference/main.cpp
+++ microbenchmarks/LinnOS/Inference/main.cpp
@@ -62,32 +62,30 @@ int main(int argc, char** argv)
      *  GPU batched timing
      */
 
-    int batch_sizes[] = {16, 64, 128, 256, 512};
+    int batch_sizes[] = {32, 64, 128, 256, 512};
     for (int &N_INPUTS_BATCH : batch_sizes) {
         setup_batch(N_INPUTS_BATCH);
         uint32_t gpubatch_total(0);
         uint32_t gpubatch_all_total(0);
 
         // //warmup
-        // for (int j = 0 ; j < n/N_INPUTS_BATCH ; j++) {
-        //     gpu_setup_inputs(linear_inputs+j*N_INPUTS_BATCH, N_INPUTS_BATCH);
-        //     gpu_inference_many(N_INPUTS_BATCH);
-        // }
+        for (int j = 0 ; j < n/N_INPUTS_BATCH ; j++) {
+            gpu_setup_inputs(linear_inputs+j*N_INPUTS_BATCH, N_INPUTS_BATCH);
+            gpu_inference_many(N_INPUTS_BATCH);
+        }
 
         //for each batch, measure
-        for (int j = 0 ; j < n/N_INPUTS_BATCH ; j++) {
-            std::chrono::steady_clock::time_point begin_gpu_all = std::chrono::steady_clock::now();
-            copy_inputs_batch(N_INPUTS_BATCH);
-            std::chrono::steady_clock::time_point begin_gpu = std::chrono::steady_clock::now();
-            infer_batch(N_INPUTS_BATCH);
-            std::chrono::steady_clock::time_point end_gpu = std::chrono::steady_clock::now();
-            get_result_batch(N_INPUTS_BATCH);
-            std::chrono::steady_clock::time_point end_gpu_all = std::chrono::steady_clock::now();
+        std::chrono::steady_clock::time_point begin_gpu_all = std::chrono::steady_clock::now();
+        copy_inputs_batch(N_INPUTS_BATCH);
+        std::chrono::steady_clock::time_point begin_gpu = std::chrono::steady_clock::now();
+        infer_batch(N_INPUTS_BATCH);
+        std::chrono::steady_clock::time_point end_gpu = std::chrono::steady_clock::now();
+        get_result_batch(N_INPUTS_BATCH);
+        std::chrono::steady_clock::time_point end_gpu_all = std::chrono::steady_clock::now();
 
-            gpubatch_total += std::chrono::duration_cast<std::chrono::nanoseconds>(end_gpu - begin_gpu).count();
-            gpubatch_all_total += std::chrono::duration_cast<std::chrono::nanoseconds>(end_gpu_all - begin_gpu_all).count();
+        gpubatch_total = (std::chrono::duration_cast<std::chrono::nanoseconds>(end_gpu - begin_gpu).count())/1000;
+        gpubatch_all_total = (std::chrono::duration_cast<std::chrono::nanoseconds>(end_gpu_all - begin_gpu_all).count())/1000;
 
-        }
         std::cout << "Batched GPU time for " << n << " inferences (batch size " << N_INPUTS_BATCH << "): " << gpubatch_total << "ns. Average per inference:" << gpubatch_total/n << "ns." << std::endl;
         std::cout << "Including data transfers: " << gpubatch_all_total << "ns. Average per inference:" << gpubatch_all_total/n << "ns." << std::endl;
         
