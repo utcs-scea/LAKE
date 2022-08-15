bc71e1fdb11a7034fce740ab20170532794a729c
Henrique Fingler <hfingler@cs.utexas.edu>
2022-08-15 23:47:52 +0000

    fix makefile

--- src/ecryptfs/crypto/.lake_gcm.mod.o.cmd
+++ src/ecryptfs/crypto/.lake_gcm.mod.o.cmd
@@ -1,8 +1,8 @@
-cmd_/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o := gcc -Wp,-MD,/home/hfingler/hf-HACK/src/ecryptfs/crypto/.lake_gcm.mod.o.d  -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/7/include -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -include ./include/linux/compiler_types.h -D__KERNEL__ -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -fshort-wchar -Werror-implicit-function-declaration -Werror=return-type -Wno-format-security -std=gnu89 -fno-PIE -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -m64 -falign-jumps=1 -falign-loops=1 -mno-80387 -mno-fp-ret-in-387 -mpreferred-stack-boundary=3 -mskip-rax-setup -mtune=generic -mno-red-zone -mcmodel=kernel -funit-at-a-time -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_FXSAVEQ=1 -DCONFIG_AS_SSSE3=1 -DCONFIG_AS_CRC32=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -DCONFIG_AS_AVX512=1 -DCONFIG_AS_SHA1_NI=1 -DCONFIG_AS_SHA256_NI=1 -pipe -Wno-sign-compare -fno-asynchronous-unwind-tables -mindirect-branch=thunk-extern -mindirect-branch-register -fno-jump-tables -fno-delete-null-pointer-checks -Wno-frame-address -Wno-format-truncation -Wno-format-overflow -Wno-int-in-bool-context -O2 --param=allow-store-data-races=0 -Wframe-larger-than=2048 -fstack-protector-strong -Wno-unused-but-set-variable -Wno-unused-const-variable -fomit-frame-pointer -fno-var-tracking-assignments -Wdeclaration-after-statement -Wno-pointer-sign -Wno-array-bounds -Wno-stringop-overflow -Wno-restrict -Wno-maybe-uninitialized -fno-strict-overflow -fno-merge-all-constants -fmerge-constants -fno-stack-check -fconserve-stack -Werror=implicit-int -Werror=strict-prototypes -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init -I/home/hfingler/hf-HACK/kava/klib/cuda -I/home/hfingler/hf-HACK/kava/include  -DKBUILD_BASENAME='"lake_gcm.mod"' -DKBUILD_MODNAME='"lake_gcm"' -DMODULE  -c -o /home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o /home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.c
+cmd_/home/hfingler/HACK/src/ecryptfs/crypto/lake_gcm.mod.o := gcc -Wp,-MD,/home/hfingler/HACK/src/ecryptfs/crypto/.lake_gcm.mod.o.d  -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/9/include -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -include ./include/linux/compiler_types.h -D__KERNEL__ -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -fshort-wchar -Werror-implicit-function-declaration -Werror=return-type -Wno-format-security -std=gnu89 -fno-PIE -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -fcf-protection=none -m64 -falign-jumps=1 -falign-loops=1 -mno-80387 -mno-fp-ret-in-387 -mpreferred-stack-boundary=3 -mskip-rax-setup -mtune=generic -mno-red-zone -mcmodel=kernel -funit-at-a-time -DCONFIG_X86_X32_ABI -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_FXSAVEQ=1 -DCONFIG_AS_SSSE3=1 -DCONFIG_AS_CRC32=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -DCONFIG_AS_AVX512=1 -DCONFIG_AS_SHA1_NI=1 -DCONFIG_AS_SHA256_NI=1 -pipe -Wno-sign-compare -fno-asynchronous-unwind-tables -mindirect-branch=thunk-extern -mindirect-branch-register -fno-jump-tables -fno-delete-null-pointer-checks -Wno-frame-address -Wno-format-truncation -Wno-format-overflow -Wno-int-in-bool-context -Wno-address-of-packed-member -O2 --param=allow-store-data-races=0 -Wframe-larger-than=1024 -fstack-protector-strong -Wno-unused-but-set-variable -Wno-unused-const-variable -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-var-tracking-assignments -g -gdwarf-4 -pg -mrecord-mcount -mfentry -DCC_USING_FENTRY -Wdeclaration-after-statement -Wno-pointer-sign -Wno-stringop-truncation -Wno-array-bounds -Wno-stringop-overflow -Wno-restrict -Wno-maybe-uninitialized -fno-strict-overflow -fno-merge-all-constants -fmerge-constants -fno-stack-check -fconserve-stack -Werror=implicit-int -Werror=strict-prototypes -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init -fmacro-prefix-map=./= -Wno-packed-not-aligned -I/home/hfingler/HACK/src/ecryptfs/crypto/../../../kava/klib/cuda -I/home/hfingler/HACK/src/ecryptfs/crypto/../../../kava/include  -DKBUILD_BASENAME='"lake_gcm.mod"' -DKBUILD_MODNAME='"lake_gcm"' -DMODULE  -c -o /home/hfingler/HACK/src/ecryptfs/crypto/lake_gcm.mod.o /home/hfingler/HACK/src/ecryptfs/crypto/lake_gcm.mod.c
 
-source_/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o := /home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.c
+source_/home/hfingler/HACK/src/ecryptfs/crypto/lake_gcm.mod.o := /home/hfingler/HACK/src/ecryptfs/crypto/lake_gcm.mod.c
 
-deps_/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o := \
+deps_/home/hfingler/HACK/src/ecryptfs/crypto/lake_gcm.mod.o := \
     $(wildcard include/config/module/unload.h) \
     $(wildcard include/config/retpoline.h) \
   include/linux/kconfig.h \
@@ -86,7 +86,7 @@ deps_/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o := \
     $(wildcard include/config/panic/timeout.h) \
     $(wildcard include/config/tracing.h) \
     $(wildcard include/config/ftrace/mcount/record.h) \
-  /usr/lib/gcc/x86_64-linux-gnu/7/include/stdarg.h \
+  /usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h \
   include/linux/linkage.h \
   include/linux/export.h \
     $(wildcard include/config/modules.h) \
@@ -158,6 +158,10 @@ deps_/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o := \
     $(wildcard include/config/x86/l1/cache/shift.h) \
     $(wildcard include/config/x86/internode/cache/shift.h) \
     $(wildcard include/config/x86/vsmp.h) \
+  include/linux/dynamic_debug.h \
+    $(wildcard include/config/jump/label.h) \
+  include/linux/jump_label.h \
+  arch/x86/include/asm/jump_label.h \
   include/linux/build_bug.h \
   include/linux/restart_block.h \
   include/linux/time64.h \
@@ -257,12 +261,8 @@ deps_/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o := \
     $(wildcard include/config/proc/fs.h) \
   arch/x86/include/asm/pgtable_64_types.h \
   arch/x86/include/asm/sparsemem.h \
-  include/asm-generic/pgtable-nop4d.h \
   arch/x86/include/asm/nospec-branch.h \
   include/linux/static_key.h \
-  include/linux/jump_label.h \
-    $(wildcard include/config/jump/label.h) \
-  arch/x86/include/asm/jump_label.h \
   arch/x86/include/asm/alternative-asm.h \
   arch/x86/include/asm/msr-index.h \
   arch/x86/include/asm/spinlock_types.h \
@@ -572,6 +572,14 @@ deps_/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o := \
   include/asm-generic/pci_iomap.h \
     $(wildcard include/config/no/generic/pci/ioport/map.h) \
     $(wildcard include/config/generic/pci/iomap.h) \
+  include/xen/xen.h \
+    $(wildcard include/config/xen/pvh.h) \
+    $(wildcard include/config/xen/dom0.h) \
+  include/xen/interface/xen.h \
+  arch/x86/include/asm/xen/interface.h \
+  arch/x86/include/asm/xen/interface_64.h \
+  arch/x86/include/asm/pvclock-abi.h \
+  arch/x86/include/asm/xen/hypervisor.h \
   include/asm-generic/io.h \
     $(wildcard include/config/virt/to/bus.h) \
   include/linux/logic_pio.h \
@@ -659,6 +667,6 @@ deps_/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o := \
   include/linux/vermagic.h \
   include/generated/utsrelease.h \
 
-/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o: $(deps_/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o)
+/home/hfingler/HACK/src/ecryptfs/crypto/lake_gcm.mod.o: $(deps_/home/hfingler/HACK/src/ecryptfs/crypto/lake_gcm.mod.o)
 
-$(deps_/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o):
+$(deps_/home/hfingler/HACK/src/ecryptfs/crypto/lake_gcm.mod.o):
--- src/ecryptfs/crypto/.tmp_versions/lake_gcm.mod
+++ src/ecryptfs/crypto/.tmp_versions/lake_gcm.mod
@@ -1,3 +1,3 @@
-/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.ko
-/home/hfingler/hf-HACK/src/ecryptfs/crypto/gcm_main.o /home/hfingler/hf-HACK/src/ecryptfs/crypto/gcm_cuda.o
+/home/hfingler/HACK/src/ecryptfs/crypto/lake_gcm.ko
+/home/hfingler/HACK/src/ecryptfs/crypto/gcm_main.o /home/hfingler/HACK/src/ecryptfs/crypto/gcm_cuda.o
 
