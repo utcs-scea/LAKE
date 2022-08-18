fb757c99db3398fa2c6059723245204dc7cc9741
Henrique Fingler <hfingler@cs.utexas.edu>
2022-08-18 17:02:20 +0000

    cleanup

--- src/ecryptfs/crypto/.lake_gcm.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.ko /home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.o /home/hfingler/hf-HACK/src/ecryptfs/crypto/lake_gcm.mod.o ;  true
