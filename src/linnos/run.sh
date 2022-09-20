d4ccbc31fa01384665a348b6b14732c1eeabd871
hfingler <henrique.fingler@gmail.com>
2022-09-20 18:36:15 -0500

    correcting run script

--- src/linnos/run.sh
+++ src/linnos/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 make
-sudo insmod linnos_kern.ko cubin_path=$(readlink -f ./linnos.cubin)
-sudo rmmod linnos_kern
+sudo insmod linnos.ko cubin_path=$(readlink -f ./linnos.cubin)
+sudo rmmod linnos
