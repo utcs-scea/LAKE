7aa0a2593e2cba6d90f33e9db672f1bacfa188c9
Henrique Fingler <hfingler@cs.utexas.edu>
2022-09-29 00:08:52 +0000

    tmux script

--- tmux.sh
+++ tmux.sh
@@ -8,7 +8,9 @@ SCRIPT_DIR=`dirname $SCRIPT`
 tmux new-session -d -c ${SCRIPT_DIR}/src/kapi
 tmux split-window -h -c ${SCRIPT_DIR}/src
 tmux select-pane -t 0
-tmux split-window -v 'dmesg -w'
+tmux split-window -v
+tmux select-pane -t 1
+tmux send-keys 'sudo dmesg -w' Enter
 tmux select-pane -t 0
 tmux send-keys 'sudo ./load.sh'
 tmux -2 attach-session -d
