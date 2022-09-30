f14cf7f2772d096f7c2eb8a455f29de3df9cf266
hfingler <henrique.fingler@gmail.com>
2022-09-30 12:12:52 -0500

    save bpf

--- src/bpf/programs/hello/hello.bpf.c
+++ src/bpf/programs/hello/hello.bpf.c
@@ -11,33 +11,27 @@
 extern __u64 bpf_kfunc_call_test1(struct sock *sk, __u32 a, __u64 b,
 				  __u32 c, __u64 d) __ksym;
 
-SEC("tp/raw_syscalls/sys_enter")
-int handle_tp(void *ctx)
-{
-	// int pid = bpf_get_current_pid_tgid() >> 32;
-	// int r;
-
-	// if (pid != my_pid)
-	// 	return 0;
-
-	// bpf_printk("BPF triggered from PID %d.\n", pid);
-
-	__u64 a;
-   	a = bpf_kfunc_call_test1(0, 1, 2, 3, 4);
-   	bpf_printk("bpf_kfunc_call_test1:  %d.\n", a);
-
-	return 0;
-}
-
-// SEC("classifier")
-// int kfunc_call_test1(struct __sk_buff *skb)
+// SEC("tp/raw_syscalls/sys_enter")
+// int handle_tp(void *ctx)
 // {
-// 	struct sock *sk = 0;
-//   	__u64 a;
-//   	a = bpf_kfunc_call_test1(sk, 1, 2, 3, 4);
-//   	bpf_printk("bpf_kfunc_call_test1:  %d.\n", a);
-//   	return a;
+// 	__u64 a;
+//    	a = bpf_kfunc_call_test1(0, 1, 2, 3, 4);
+//    	bpf_printk("bpf_kfunc_call_test1:  %d.\n", a);
+
+// 	return 0;
 // }
+
+SEC("classifier")
+int kfunc_call_test1(struct __sk_buff *skb)
+{
+	//struct sock *sk = 0;
+  	struct bpf_sock *sk = skb->sk;
+	sk = bpf_sk_fullsock(sk);
+	__u64 a;
+  	a = bpf_kfunc_call_test1(sk, 1, 2, 3, 4);
+  	bpf_printk("bpf_kfunc_call_test1:  %d.\n", a);
+  	return a;
+}
 
 
 char _license[] SEC("license") = "GPL";
