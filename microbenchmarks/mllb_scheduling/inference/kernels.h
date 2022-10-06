81c91d7286987b0a1fe1864f88de436b52648f9b
Henrique Fingler <hfingler@cs.utexas.edu>
2022-10-06 21:38:44 +0000

    cleanup

--- microbenchmarks/mllb_scheduling/inference/kernels.h
+++
@@ -1,8 +0,0 @@
-
-
-void gpu_setup(int n_inputs);
-void gpu_clean();
-void gpu_setup_inputs(float* inputs, int n);
-float gpu_inference();
-float gpu_inference_many(int n_inputs);
-float gpu_get_result(int n_inputs);
