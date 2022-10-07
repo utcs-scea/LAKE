377de6b5d54de834d5377611ba5fdec12da4ed3a
Isha Tarte <tarteisha@gmail.com>
2022-10-07 21:55:26 +0000

    add cpu code in src/linnos

--- src/linnos/main.c
+++ src/linnos/main.c
@@ -25,7 +25,7 @@ u64 get_tsns() {
 #define ktime_get_ns() get_tsns()
 #endif
 
-
+#define FEAT_31
 #include <stdbool.h>
 #include "weights.h"
 #include "helpers.h"
@@ -47,6 +47,111 @@ MODULE_PARM_DESC(cubin_path, "The path to linnos.cubin, default ./linnos.cubin")
 
 static int run_cpu(void) {
     return 0;
+}
+
+static bool cpu_prediction_model(long *feat_vec) {
+
+	long input_vec_i[LEN_INPUT], mid_res_i[LEN_LAYER_0], final_res_i[LEN_LAYER_1];
+	long *weight_0_T_ent, * bias_0_ent, *weight_1_T_ent, * bias_1_ent; 
+	int i, j, k, offset;
+
+	for (i=0 ; i<LEN_INPUT; i++) {
+		input_vec_i[i] = (long)(feat_vec[i]);
+		// input_vec_i[i] = (long)(test_input[index][i]);
+	}
+
+    weight_0_T_ent = &weight_i_0_T[0][0];
+    weight_1_T_ent = &weight_i_1[0][0];
+    bias_0_ent = bias_i_0;
+    bias_1_ent = bias_i_1;
+
+	for (j = 0, offset=0; j < LEN_LAYER_0; j++, offset+=LEN_INPUT) {
+        mid_res_i[j] = 0;
+        //loop unroll
+#ifdef FEAT_31
+
+		mid_res_i[j] += (input_vec_i[0] == 0 || weight_0_T_ent[offset+0] == 0)? 0 : input_vec_i[0] * weight_0_T_ent[offset+0];
+		mid_res_i[j] += (input_vec_i[1] == 0 || weight_0_T_ent[offset+1] == 0)? 0 : input_vec_i[1] * weight_0_T_ent[offset+1];
+		mid_res_i[j] += (input_vec_i[2] == 0 || weight_0_T_ent[offset+2] == 0)? 0 : input_vec_i[2] * weight_0_T_ent[offset+2];
+		mid_res_i[j] += (input_vec_i[3] == 0 || weight_0_T_ent[offset+3] == 0)? 0 : input_vec_i[3] * weight_0_T_ent[offset+3];
+		mid_res_i[j] += (input_vec_i[4] == 0 || weight_0_T_ent[offset+4] == 0)? 0 : input_vec_i[4] * weight_0_T_ent[offset+4];
+		mid_res_i[j] += (input_vec_i[5] == 0 || weight_0_T_ent[offset+5] == 0)? 0 : input_vec_i[5] * weight_0_T_ent[offset+5];
+		mid_res_i[j] += (input_vec_i[6] == 0 || weight_0_T_ent[offset+6] == 0)? 0 : input_vec_i[6] * weight_0_T_ent[offset+6];
+		mid_res_i[j] += (input_vec_i[7] == 0 || weight_0_T_ent[offset+7] == 0)? 0 : input_vec_i[7] * weight_0_T_ent[offset+7];
+		mid_res_i[j] += (input_vec_i[8] == 0 || weight_0_T_ent[offset+8] == 0)? 0 : input_vec_i[8] * weight_0_T_ent[offset+8];
+		mid_res_i[j] += (input_vec_i[9] == 0 || weight_0_T_ent[offset+9] == 0)? 0 : input_vec_i[9] * weight_0_T_ent[offset+9];
+		mid_res_i[j] += (input_vec_i[10] == 0 || weight_0_T_ent[offset+10] == 0)? 0 : input_vec_i[10] * weight_0_T_ent[offset+10];
+		mid_res_i[j] += (input_vec_i[11] == 0 || weight_0_T_ent[offset+11] == 0)? 0 : input_vec_i[11] * weight_0_T_ent[offset+11];
+		mid_res_i[j] += (input_vec_i[12] == 0 || weight_0_T_ent[offset+12] == 0)? 0 : input_vec_i[12] * weight_0_T_ent[offset+12];
+		mid_res_i[j] += (input_vec_i[13] == 0 || weight_0_T_ent[offset+13] == 0)? 0 : input_vec_i[13] * weight_0_T_ent[offset+13];
+		mid_res_i[j] += (input_vec_i[14] == 0 || weight_0_T_ent[offset+14] == 0)? 0 : input_vec_i[14] * weight_0_T_ent[offset+14];
+		mid_res_i[j] += (input_vec_i[15] == 0 || weight_0_T_ent[offset+15] == 0)? 0 : input_vec_i[15] * weight_0_T_ent[offset+15];
+		mid_res_i[j] += (input_vec_i[16] == 0 || weight_0_T_ent[offset+16] == 0)? 0 : input_vec_i[16] * weight_0_T_ent[offset+16];
+		mid_res_i[j] += (input_vec_i[17] == 0 || weight_0_T_ent[offset+17] == 0)? 0 : input_vec_i[17] * weight_0_T_ent[offset+17];
+		mid_res_i[j] += (input_vec_i[18] == 0 || weight_0_T_ent[offset+18] == 0)? 0 : input_vec_i[18] * weight_0_T_ent[offset+18];
+		mid_res_i[j] += (input_vec_i[19] == 0 || weight_0_T_ent[offset+19] == 0)? 0 : input_vec_i[19] * weight_0_T_ent[offset+19];
+		mid_res_i[j] += (input_vec_i[20] == 0 || weight_0_T_ent[offset+20] == 0)? 0 : input_vec_i[20] * weight_0_T_ent[offset+20];
+		mid_res_i[j] += (input_vec_i[21] == 0 || weight_0_T_ent[offset+21] == 0)? 0 : input_vec_i[21] * weight_0_T_ent[offset+21];
+		mid_res_i[j] += (input_vec_i[22] == 0 || weight_0_T_ent[offset+22] == 0)? 0 : input_vec_i[22] * weight_0_T_ent[offset+22];
+		mid_res_i[j] += (input_vec_i[23] == 0 || weight_0_T_ent[offset+23] == 0)? 0 : input_vec_i[23] * weight_0_T_ent[offset+23];
+		mid_res_i[j] += (input_vec_i[24] == 0 || weight_0_T_ent[offset+24] == 0)? 0 : input_vec_i[24] * weight_0_T_ent[offset+24];
+		mid_res_i[j] += (input_vec_i[25] == 0 || weight_0_T_ent[offset+25] == 0)? 0 : input_vec_i[25] * weight_0_T_ent[offset+25];
+		mid_res_i[j] += (input_vec_i[26] == 0 || weight_0_T_ent[offset+26] == 0)? 0 : input_vec_i[26] * weight_0_T_ent[offset+26];
+		mid_res_i[j] += (input_vec_i[27] == 0 || weight_0_T_ent[offset+27] == 0)? 0 : input_vec_i[27] * weight_0_T_ent[offset+27];
+		mid_res_i[j] += (input_vec_i[28] == 0 || weight_0_T_ent[offset+28] == 0)? 0 : input_vec_i[28] * weight_0_T_ent[offset+28];
+		mid_res_i[j] += (input_vec_i[29] == 0 || weight_0_T_ent[offset+29] == 0)? 0 : input_vec_i[29] * weight_0_T_ent[offset+29];
+		mid_res_i[j] += (input_vec_i[30] == 0 || weight_0_T_ent[offset+30] == 0)? 0 : input_vec_i[30] * weight_0_T_ent[offset+30];
+
+#else
+        // if k%4==0
+        for (k = 0; k < LEN_INPUT; k += 4) {
+            // mid_res_i[j] += input_vec_i[k] * weight_i_0[k][j];
+            // mid_res_i[j] += input_vec_i[k+1] * weight_i_0[k+1][j];
+            // mid_res_i[j] += input_vec_i[k+2] * weight_i_0[k+2][j];
+            // mid_res_i[j] += input_vec_i[k+3] * weight_i_0[k+3][j];
+            mid_res_i[j] += (input_vec_i[k] == 0 || weight_0_T_ent[k][j] == 0)? 0 : input_vec_i[k] * weight_0_T_ent[k][j];
+            mid_res_i[j] += (input_vec_i[k+1] == 0 || weight_0_T_ent[k+1][j] == 0)? 0 : input_vec_i[k+1] * weight_0_T_ent[k+1][j];
+            mid_res_i[j] += (input_vec_i[k+2] == 0 || weight_0_T_ent[k+2][j] == 0)? 0 : input_vec_i[k+2] * weight_0_T_ent[k+2][j];
+            mid_res_i[j] += (input_vec_i[k+3] == 0 || weight_0_T_ent[k+3][j] == 0)? 0 : input_vec_i[k+3] * weight_0_T_ent[k+3][j];
+        }
+#endif
+        // apply bias
+        mid_res_i[j] += bias_0_ent[j];
+        // relu
+        if (mid_res_i[j] < 0) {
+            mid_res_i[j] = 0;
+        }
+    }
+    
+    final_res_i[0] = 0;
+    for(k=0; k<LEN_LAYER_0; k += 8) {
+        final_res_i[0] += (mid_res_i[k] == 0 || weight_1_T_ent[k] == 0)? 0 : mid_res_i[k] * weight_1_T_ent[k];
+		final_res_i[0] += (mid_res_i[k+1] == 0 || weight_1_T_ent[k+1] == 0)? 0 : mid_res_i[k+1] * weight_1_T_ent[k+1];
+		final_res_i[0] += (mid_res_i[k+2] == 0 || weight_1_T_ent[k+2] == 0)? 0 : mid_res_i[k+2] * weight_1_T_ent[k+2];
+		final_res_i[0] += (mid_res_i[k+3] == 0 || weight_1_T_ent[k+3] == 0)? 0 : mid_res_i[k+3] * weight_1_T_ent[k+3];
+		final_res_i[0] += (mid_res_i[k+4] == 0 || weight_1_T_ent[k+4] == 0)? 0 : mid_res_i[k+4] * weight_1_T_ent[k+4];
+		final_res_i[0] += (mid_res_i[k+5] == 0 || weight_1_T_ent[k+5] == 0)? 0 : mid_res_i[k+5] * weight_1_T_ent[k+5];
+		final_res_i[0] += (mid_res_i[k+6] == 0 || weight_1_T_ent[k+6] == 0)? 0 : mid_res_i[k+6] * weight_1_T_ent[k+6];
+		final_res_i[0] += (mid_res_i[k+7] == 0 || weight_1_T_ent[k+7] == 0)? 0 : mid_res_i[k+7] * weight_1_T_ent[k+7];
+	}
+	// apply bias
+	final_res_i[0] += bias_1_ent[0];
+
+	final_res_i[1] = 0;
+    for(k=0; k<LEN_LAYER_0; k += 8) {
+        final_res_i[1] += (mid_res_i[k] == 0 || weight_1_T_ent[k+256] == 0)? 0 : mid_res_i[k] * weight_1_T_ent[k+256];
+		final_res_i[1] += (mid_res_i[k+1] == 0 || weight_1_T_ent[k+257] == 0)? 0 : mid_res_i[k+1] * weight_1_T_ent[k+257];
+		final_res_i[1] += (mid_res_i[k+2] == 0 || weight_1_T_ent[k+258] == 0)? 0 : mid_res_i[k+2] * weight_1_T_ent[k+258];
+		final_res_i[1] += (mid_res_i[k+3] == 0 || weight_1_T_ent[k+259] == 0)? 0 : mid_res_i[k+3] * weight_1_T_ent[k+259];
+		final_res_i[1] += (mid_res_i[k+4] == 0 || weight_1_T_ent[k+260] == 0)? 0 : mid_res_i[k+4] * weight_1_T_ent[k+260];
+		final_res_i[1] += (mid_res_i[k+5] == 0 || weight_1_T_ent[k+261] == 0)? 0 : mid_res_i[k+5] * weight_1_T_ent[k+261];
+		final_res_i[1] += (mid_res_i[k+6] == 0 || weight_1_T_ent[k+262] == 0)? 0 : mid_res_i[k+6] * weight_1_T_ent[k+262];
+		final_res_i[1] += (mid_res_i[k+7] == 0 || weight_1_T_ent[k+263] == 0)? 0 : mid_res_i[k+7] * weight_1_T_ent[k+263];
+	}
+	// apply bias
+	final_res_i[1] += bias_1_ent[1];
+
+    return final_res_i[0]>=(final_res_i[1])? false: true;
 }
 
 static inline void check_malloc(void *p, const char* error_str, int line) {
@@ -286,15 +391,42 @@ static int run_gpu(void) {
 	}
 
     if(printResults) {
-        int batch_size = 4;
-        flatten_input_test_output(4, input);
-        setup_gpu(batch_size);    
-        copy_batch_inputs(batch_size);
-        gpu_inference(&batch_linnos_mid_layer_kernel, &batch_linnos_final_layer_kernel, batch_size, 1);
-        get_result_batch(batch_size);
-        cuCtxSynchronize();
-
-        print_results(batch_size);
+        int batch_size = 1;
+        long input[4][31] = {
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,9,0,0,0,9,0,0,0,9},
+            {9,9,9,0,9,1,1,9,9,9,9,0,9,9,1,9,9,1,9,9,0,9,1,9,1,9,0,9,9,0,9},
+            {9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+        };
+        for(int k = 0; k < 4; k++) {
+            setup_gpu(batch_size);
+            for(j = 0; j < 31; j++)
+		        parallel_input[ j ] = input[k][j];    
+            copy_batch_inputs(batch_size);
+            gpu_inference(&batch_linnos_mid_layer_kernel, &batch_linnos_final_layer_kernel, batch_size, 1);
+            get_result_batch(batch_size);
+            cuCtxSynchronize();
+            int cpu_result = cpu_prediction_model(parallel_input);
+            //printf("cpu result %d\n", cpu_result);
+            #ifdef __KERNEL__
+                PRINT(V_INFO, "GPU results %d,\n", res[0]);
+                PRINT(V_INFO, "CPU results %d,\n", cpu_result);
+                if (res[0] == cpu_result) {
+                    PRINT(V_INFO, "Equal! \n");
+                } else {
+                    PRINT(V_INFO, " Not Equal! \n");
+                }
+            #else
+                printf("GPU results %d\n", res[0]);
+                printf("CPU results %d\n", cpu_result);
+                if (res[0] == cpu_result) {
+                    printf("Equal! \n");
+                } else {
+                    printf("Not Equal! \n");
+                }
+            #endif
+        }
+        
     }
 
     cleanup();
