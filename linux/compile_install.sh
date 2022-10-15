7c1c6c9b8da6d220da6f85fbf972c0a5ca81f82d
Henrique Fingler <hfingler@cs.utexas.edu>
2022-10-15 21:54:12 +0000

    better shim

--- linux/compile_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-make -j16
-sudo make INSTALL_MOD_STRIP=1 modules_install
-#sudo make headers_install INSTALL_HDR_PATH=/usr
-sudo make install
--- toys/io_tracer/shim/constructor.cpp
+++
@@ -1,8 +0,0 @@
-#include "reads.h"
-#include "mmap.h"
-
-void __attribute__((constructor)) initialize(void) {
-
-    reads_contructor();
-    //mmap_contructor();
-}
