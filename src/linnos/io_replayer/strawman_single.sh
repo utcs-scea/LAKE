555eaeae348ac27e9a1809e4b849191a131277b6
Henrique Fingler <hfingler@cs.utexas.edu>
2022-10-25 19:22:23 +0000

    scripts for cpu +1 +2

--- src/linnos/io_replayer/strawman_single.sh
+++ src/linnos/io_replayer/strawman_single.sh
@@ -9,8 +9,6 @@ fi
 
 sudo ./replayer $1 xxx 1 /dev/nvme0n1-/dev/nvme1n1 $2
 
-
-
 #./strawman.sh strawman ../trace_tools/15s_256k_50us.trace
 #./strawman.sh strawman ../trace_tools/15s_1m_100us.trace
 
