6fe8f4ac50d4ae8eff773dc1c88688d88d21213f
Henrique Fingler <hfingler@cs.utexas.edu>
2022-10-30 20:32:24 +0000

    api thread

--- benchmarks/utilization/tools/nprocs_gpu.cpp
+++ benchmarks/utilization/tools/nprocs_gpu.cpp
@@ -4,26 +4,13 @@
 #include <thread>
 #include <atomic>
 #include <stdlib.h>
-#include <cstdio>
-#include <iostream>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <array>
-#include <cctype>
-#include <algorithm>
-#include <string>
 
 //https://gist.githubusercontent.com/sakamoto-poteko/44d6cd19552fa7721b99/raw/4098c76ec7258c2d548cff47a0b8d6c5a6286e4e/nvml.cpp
 
 const int interval_ms = 1000;
 
-
 static std::atomic<float> last_cpu;
 static std::atomic<float> last_gpu;
-static std::atomic<float> last_api;
-static std::atomic<uint64_t> total_cpu1;
-static std::atomic<uint64_t> total_cpu2;
 
 struct cpustat {
     unsigned long t_user;
@@ -79,14 +66,6 @@ double calculate_load(struct cpustat *prev, struct cpustat *cur)
     double idled = (double) idle_cur - (double) idle_prev;
     double cpu_perc = (1000 * (totald - idled) / totald + 1) / 10;
 
-    //printf("sum %d\n", cur->t_user + cur->t_nice + cur->t_system + cur->t_idle);
-    uint64_t tc = cur->t_user + cur->t_nice + cur->t_system + cur->t_idle;
-    uint64_t tmp = total_cpu1.load();
-    total_cpu1.store(tc);
-    total_cpu2.store(tmp);
-
-    //printf("diff %d\n", tc - tmp);
-
     return cpu_perc;
 }
 
@@ -105,56 +84,6 @@ void cpu_thread() {
     }
 }
 
-void pid_thread() {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::string cmd = "pidof lake_uspace";
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        result = buffer.data();
-        std::cout << result;
-    }
-
-    result.erase(std::remove_if(result.begin(), result.end(), 
-                  [&](char ch) 
-                  { return std::iscntrl(static_cast<unsigned char>(ch));}), 
-                  result.end());
-
-    char pidstat[1024];
-    sprintf(pidstat, "/proc/%s/stat", result.c_str());
-
-    float t1, t2;
-
-    while(1) {
-        FILE *fp = fopen(pidstat, "r");
-        char cpun[255];
-        int pid;
-        uint64_t ll, user, system;
-        char c;
-        fscanf(fp,
-            "%d %s %c %d" //pid,command,state,ppid
-            "%d %d %d %d %u %lu %lu %lu %lu"
-            "%lu %lu", 
-            &pid, cpun, &c, &pid,
-            &pid, &pid, &pid, &pid, &pid, &ll, &ll, &ll, &ll,
-            &user, &system);
-        fclose(fp);
-
-        //printf("+: %f\n", (user+system));
-        t1 = user+system;
-        uint64_t c1 = total_cpu1.load();
-        uint64_t c2 = total_cpu2.load();
-        double pct = ((t1+t2)*100) / (c1 - c2);
-        t2 = t1;
-        last_api.store(pct);
-        std::this_thread::sleep_for(std::chrono::milliseconds(interval_ms));
-    }
-
-	return;
-}
 
 void gpu_thread() {
     nvmlReturn_t result;
@@ -177,16 +106,19 @@ void gpu_thread() {
     result = nvmlDeviceGetName(device, device_name, NVML_DEVICE_NAME_BUFFER_SIZE);
     if (result != NVML_SUCCESS)
         exit(1);
-    //std::printf("Device %d: %s\n", 0, device_name);
+    std::printf("Device %d: %s\n", 0, device_name);
 
     nvmlUtilization_st device_utilization;
 
+    unsigned int np=10;
+    nvmlProcessInfo_t pis[10];
     while(1) {
-        result = nvmlDeviceGetUtilizationRates(device, &device_utilization);
+        np=10;
+        result=nvmlDeviceGetComputeRunningProcesses(device , &np, pis);
         if (result != NVML_SUCCESS)
             exit(1);
-        last_gpu.store(2);
-        //printf("gpu %f\n", device_utilization.gpu);
+       
+        printf("procs: %d\n", np);
         std::this_thread::sleep_for(std::chrono::milliseconds(interval_ms));
     }
 
@@ -196,17 +128,15 @@ void gpu_thread() {
 int main() {
     std::thread gpu_t(gpu_thread);
     std::thread cpu_t(cpu_thread);
-    std::thread pid_t(pid_thread);
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     float ts = 0;
 
-    float c, g, a;        
+    float c, g;        
     while(1) {
         std::this_thread::sleep_for(std::chrono::milliseconds(interval_ms));
         c = last_cpu.load();
         g = last_gpu.load();
-        a = last_api.load();
-        printf("%.2f,%.2f,%.2f,%.2f\n", ts, c, g, a);
+        printf("%.2f,%.2f,%.2f\n", ts, c, g);
         ts += interval_ms;
     }
     return 0;
