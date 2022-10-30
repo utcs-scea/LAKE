ce4b7e3dd70ad1bdb251369db793f5f5d2636065
Henrique Fingler <hfingler@cs.utexas.edu>
2022-10-30 05:41:57 +0000

    whoops

--- benchmarks/utilization/tools/cpu_gpu.cpp
+++ benchmarks/utilization/tools/cpu_gpu.cpp
@@ -50,15 +50,15 @@ void get_stats(struct cpustat *st, int cpunum)
 
 double calculate_load(struct cpustat *prev, struct cpustat *cur)
 {
-    int idle_prev = (prev->t_idle) + (prev->t_iowait);
-    int idle_cur = (cur->t_idle) + (cur->t_iowait);
-    //int idle_prev = (prev->t_idle) + (prev->t_user) + (prev->t_nice + (prev->t_iowait));
-    //int idle_cur = (cur->t_idle) + (cur->t_user) + (cur->t_nice) + (cur->t_iowait);
+    //int idle_prev = (prev->t_idle) + (prev->t_iowait);
+    //int idle_cur = (cur->t_idle) + (cur->t_iowait);
+    int idle_prev = (prev->t_idle) + (prev->t_user) + (prev->t_nice) + (prev->t_iowait);
+    int idle_cur = (cur->t_idle) + (cur->t_user) + (cur->t_nice) + (cur->t_iowait);
 
-    int nidle_prev = (prev->t_user) + (prev->t_nice) + (prev->t_system) + (prev->t_irq) + (prev->t_softirq);
-    int nidle_cur = (cur->t_user) + (cur->t_nice) + (cur->t_system) + (cur->t_irq) + (cur->t_softirq);
-    //int nidle_prev = (prev->t_system) + (prev->t_irq) + (prev->t_softirq); //+ (prev->t_iowait);
-    //int nidle_cur = (cur->t_system) + (cur->t_irq) + (cur->t_softirq); //+ (cur->t_iowait);
+    //int nidle_prev = (prev->t_user) + (prev->t_nice) + (prev->t_system) + (prev->t_irq) + (prev->t_softirq);
+    //int nidle_cur = (cur->t_user) + (cur->t_nice) + (cur->t_system) + (cur->t_irq) + (cur->t_softirq);
+    int nidle_prev = (prev->t_system) + (prev->t_irq) + (prev->t_softirq); //+ (prev->t_iowait);
+    int nidle_cur = (cur->t_system) + (cur->t_irq) + (cur->t_softirq); //+ (cur->t_iowait);
     
     int total_prev = idle_prev + nidle_prev;
     int total_cur = idle_cur + nidle_cur;
