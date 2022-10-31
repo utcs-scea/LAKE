50e585f7653171a903bccf5ceb3060708455824d
hfingler <henrique.fingler@gmail.com>
2022-10-31 17:20:10 -0500

    fixing ecryptfs cpu utilization graph

--- benchmarks/utilization/run.py
+++ benchmarks/utilization/run.py
@@ -229,23 +229,12 @@ for name, args in tests.items():
         x = np.loadtxt('tmp.out', dtype=float, delimiter=',',skiprows=0,usecols=(0,))
         np.save("x_data.bin", x)
         x = x/1000
-        x = x[np.where(x > 5)] # we wait 5 seconds, cut the first 2
-        x = x - x[0] #offset to zero
-        #print("x: ", x)
         cpu =  np.loadtxt('tmp.out', dtype=float, delimiter=',',skiprows=0,usecols=(1,))
         np.save("cpu_data.bin", cpu)
-        #print(f"cpu: ", cpu)
-        cpu = cpu[-x.shape[0]:]
-        #print(f"cpu sliced: ", cpu)
     if name == "AESNI":
         aes_ni = np.loadtxt('tmp.out', dtype=float, delimiter=',',skiprows=0,usecols=(1,))
         np.save("aes_ni_data.bin", aes_ni)
         #print("aes: ", aes_ni)
-        aes_ni = aes_ni[-x.shape[0]:]
-        pad = x.shape[0] - aes_ni.shape[0]
-        if pad >0:
-            aes_ni = np.pad(aes_ni, (0, pad), 'constant')
-        #print("aes sliced: ", aes_ni)
     if name == "LAKE":
         lake_cpu = np.loadtxt('tmp.out', dtype=float, delimiter=',',skiprows=0,usecols=(1,))
         lake_gpu = np.loadtxt('tmp.out', dtype=float, delimiter=',',skiprows=0,usecols=(2,))
@@ -253,22 +242,6 @@ for name, args in tests.items():
         np.save("lake_cpu_data.bin", lake_cpu)
         np.save("lake_gpu_data.bin", lake_gpu)
         np.save("lake_api_data.bin", lake_api)
-
-        lake_cpu = lake_cpu[-x.shape[0]:]
-        pad = x.shape[0] - lake_cpu.shape[0]
-        if pad >0:
-            lake_cpu = np.pad(lake_cpu, (0, pad), 'constant')
-
-        lake_gpu = lake_gpu[-x.shape[0]:]
-        pad = x.shape[0] - lake_gpu.shape[0]
-        if pad >0:
-            lake_gpu = np.pad(lake_gpu, (0, pad), 'constant')
-
-        lake_api = lake_api[-x.shape[0]:]
-        pad = x.shape[0] - lake_api.shape[0]
-        if pad >0:
-            lake_api = np.pad(lake_api, (0, pad), 'constant')
-
 
     #run(f"sudo rm tmp.out", shell=True, stdout=DEVNULL)
     sleep(1)
@@ -282,6 +255,51 @@ for name, args in tests.items():
 cmap = matplotlib.cm.get_cmap("tab10")
 fig, ax = plt.subplots()
 
+#Padding with 0
+pad = x.shape[0] - cpu.shape[0]
+if pad >0:
+    cpu = np.pad(cpu, (0, pad), 'constant')
+
+pad = x.shape[0] - lake_cpu.shape[0]
+if pad >0:
+    lake_cpu = np.pad(lake_cpu, (0, pad), 'constant')
+
+pad = x.shape[0] - aes_ni.shape[0]
+if pad >0:
+    aes_ni = np.pad(aes_ni, (0, pad), 'constant')
+
+pad = x.shape[0] - lake_gpu.shape[0]
+if pad >0:
+    lake_gpu = np.pad(lake_gpu, (0, pad), 'constant')
+
+pad = x.shape[0] - lake_api.shape[0]
+if pad >0:
+    lake_api = np.pad(lake_api, (0, pad), 'constant')
+
+#cutiing the array
+index = 0
+for ele in x:
+    if ele > 8:
+        break
+    index += 1
+
+x = x[index:]
+x = x - x[0] # normalizing
+cpu = cpu[index:]
+aes_ni = aes_ni[index:]
+lake_cpu = lake_cpu[index:]
+lake_gpu = lake_gpu[index:]
+lake_api = lake_api[index:]
+
+#smoothening the grpah
+kernel_size = 3
+kernel = np.ones(kernel_size) / kernel_size
+cpu = np.convolve(cpu, kernel, mode='same')
+aes_ni = np.convolve(aes_ni, kernel, mode='same')
+lake_cpu = np.convolve(lake_cpu, kernel, mode='same')
+lake_api = np.convolve(lake_api, kernel, mode='same')
+lake_gpu = np.convolve(lake_gpu, kernel, mode='same')
+
 ax.plot(x, cpu, label='CPU', color=cmap(0) )#, marker="x", markersize=3)
 ax.plot(x, aes_ni, label='AES-NI', color=cmap(1), marker="o", markersize=3)
 ax.plot(x, lake_cpu, label='LAKE CPU', color=cmap(2), marker="v", markersize=3)
@@ -290,13 +308,13 @@ ax.plot(x, lake_gpu, label='LAKE GPU', color='black', linewidth=1, marker='.')
 #     linestyle='dotted', linewidth=3)
     
 fig.tight_layout()
-plt.xlim(left=0)
+plt.xlim(left=0, right=x[-5])
 plt.ylim(bottom=0)
 
 ax.legend(loc='upper center', ncol=1, bbox_to_anchor=(1.25, 1), columnspacing=1)
 
 ax.set_xlabel('Time (s)')
-ax.set_ylabel('Utilization (\%)')
+ax.set_ylabel('Utilization (%)')
 
 ax.grid(visible=True, which='major', axis='y', color='#0A0A0A', linestyle='--', alpha=0.2)
 
